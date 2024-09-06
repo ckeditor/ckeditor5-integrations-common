06fc4a6c40750081568af36e52f8df888350b5a2
Mateusz Baginski <cziken58@gmail.com>
2024-09-06 15:29:50 +0200

    Narrow typing of `loadCKEditorCloud` result

---
+++ src/types/ConditionalBlank.ts
@@ -0,0 +1,13 @@
+/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/**
+ * A type that allows to conditionally set a value or leave it blank.
+ * It's useful in case when we provided config and depending on the config we want to set a value or leave it blank.
+ */
+export type ConditionalBlank<Condition, Value> =
+	Condition extends true ? NonNullable<Value> :
+	Condition extends boolean ? NonNullable<Value> | undefined :
+	undefined;
